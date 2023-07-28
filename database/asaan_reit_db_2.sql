d26f54e25325d59baf5bd43b3171f03e4085f1f9
Mohsin Iqbal <mohsiniqbal185@gmail.com>
2023-07-28 17:45:42 +0500

    made further changes to database

--- database/asaan_reit_db_2.sql
+++ database/asaan_reit_db_2.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 22, 2023 at 10:32 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.0.28
+-- Generation Time: Jul 28, 2023 at 02:43 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 7.3.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,8 +18,59 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `asaan_reit_db`
+-- Database: `asaan_reit_db_2`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_user`
+--
+
+CREATE TABLE `admin_user` (
+  `user_id` int(11) NOT NULL,
+  `fname` varchar(50) DEFAULT NULL,
+  `lname` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `CNIC` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `payment_id` int(11) NOT NULL,
+  `payment_method` varchar(50) DEFAULT NULL,
+  `payment_from` int(11) DEFAULT NULL,
+  `DateTime` date DEFAULT NULL,
+  `transaction_id` int(11) DEFAULT NULL,
+  `depositor_account` varchar(50) DEFAULT NULL,
+  `payment_receipt_address` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment_status`
+--
+
+CREATE TABLE `payment_status` (
+  `payment_status_id` int(11) NOT NULL,
+  `payment_status` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `payment_status`
+--
+
+INSERT INTO `payment_status` (`payment_status_id`, `payment_status`) VALUES
+(1, 'PENDING'),
+(2, 'BEING PROCESSED'),
+(3, 'VERIFIED');
 
 -- --------------------------------------------------------
 
@@ -31,22 +82,22 @@ CREATE TABLE `property` (
   `property_id` int(11) NOT NULL,
   `property_type` varchar(50) DEFAULT NULL,
   `value_in_pkr` decimal(10,2) DEFAULT NULL,
-  `no_of_tokens` int(11) DEFAULT NULL,
+  `token_id` int(11) DEFAULT NULL,
   `tokens_sold` int(11) DEFAULT NULL,
   `size_of_property` decimal(10,2) DEFAULT NULL,
   `location` varchar(100) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   `image` varchar(100) DEFAULT NULL,
   `property_code` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `property`
 --
 
-INSERT INTO `property` (`property_id`, `property_type`, `value_in_pkr`, `no_of_tokens`, `tokens_sold`, `size_of_property`, `location`, `name`, `image`, `property_code`) VALUES
-(1, 'rental', 15000000.00, 1000, 100, 500.00, 'Chowk Waris Khan Murree Road. Rawalpindi. Punjab.', 'Kareem Plaza', '/assets/property1.jpg', 'KP'),
-(2, 'rental', 25000000.00, 2000, 200, 600.00, 'C-192, North Nazimabad, Karachi, Sindh.', 'Shah Jehan Plaza', '/assets/property2.jpg', 'SJP');
+INSERT INTO `property` (`property_id`, `property_type`, `value_in_pkr`, `token_id`, `tokens_sold`, `size_of_property`, `location`, `name`, `image`, `property_code`) VALUES
+(1, 'rental', '15000000.00', 1, 20, '500.00', 'Chowk Waris Khan Murree Road. Rawalpindi. Punjab.', 'Kareem Plaza', '/assets/property1.jpg', 'KP'),
+(2, 'rental', '25000000.00', 2, 200, '600.00', 'C-192, North Nazimabad, Karachi, Sindh.', 'Shah Jehan Plaza', '/assets/property2.jpg', 'SJP');
 
 -- --------------------------------------------------------
 
@@ -58,7 +109,7 @@ CREATE TABLE `rent` (
   `id` int(11) NOT NULL,
   `property_id` int(11) DEFAULT NULL,
   `rent_amount` decimal(10,2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -69,7 +120,7 @@ CREATE TABLE `rent` (
 CREATE TABLE `rental_month` (
   `month_id` int(11) NOT NULL,
   `month_name` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `rental_month`
@@ -104,7 +155,7 @@ CREATE TABLE `rental_transactions` (
   `rental_month_id` int(11) DEFAULT NULL,
   `rental_year_id` int(11) DEFAULT NULL,
   `rental_amount` decimal(10,2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -115,7 +166,7 @@ CREATE TABLE `rental_transactions` (
 CREATE TABLE `rental_year` (
   `year_id` int(11) NOT NULL,
   `year_name` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -126,7 +177,7 @@ CREATE TABLE `rental_year` (
 CREATE TABLE `request_status` (
   `status_id` int(11) NOT NULL,
   `name` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `request_status`
@@ -148,7 +199,7 @@ CREATE TABLE `smart_contracts` (
   `property_id` int(11) DEFAULT NULL,
   `deployer_address` varchar(150) DEFAULT NULL,
   `contract_address` varchar(150) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -162,7 +213,15 @@ CREATE TABLE `tokens` (
   `token_symbol` varchar(10) DEFAULT NULL,
   `token_value` decimal(10,2) DEFAULT NULL,
   `total_supply` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tokens`
+--
+
+INSERT INTO `tokens` (`token_id`, `token_name`, `token_symbol`, `token_value`, `total_supply`) VALUES
+(1, 'Dolmen Mall Reit Token', 'DMRT', '1.00', 100),
+(2, 'Ocean Mall Reit Token', 'OMRT', '2.00', 200);
 
 -- --------------------------------------------------------
 
@@ -176,16 +235,18 @@ CREATE TABLE `token_buy_request` (
   `property_id` int(11) DEFAULT NULL,
   `no_of_tokens` int(11) DEFAULT NULL,
   `date_of_request` datetime DEFAULT current_timestamp(),
-  `status` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `status` int(11) DEFAULT NULL,
+  `payment_status` int(11) DEFAULT NULL,
+  `payment_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `token_buy_request`
 --
 
-INSERT INTO `token_buy_request` (`req_id`, `user_id`, `property_id`, `no_of_tokens`, `date_of_request`, `status`) VALUES
-(1, 9, 1, 2, '2023-07-23 01:22:22', 0),
-(2, 8, 1, 7, '2023-07-23 01:23:14', 0);
+INSERT INTO `token_buy_request` (`req_id`, `user_id`, `property_id`, `no_of_tokens`, `date_of_request`, `status`, `payment_status`, `payment_id`) VALUES
+(1, 9, 1, 2, '2023-07-23 01:22:22', 1, 0, NULL),
+(2, 8, 1, 7, '2023-07-23 01:23:14', 0, 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -197,7 +258,7 @@ CREATE TABLE `token_holders` (
   `user_id` int(11) NOT NULL,
   `property_id` int(11) NOT NULL,
   `no_of_tokens` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `token_holders`
@@ -221,7 +282,7 @@ CREATE TABLE `token_transactions` (
   `no_of_tokens` int(11) DEFAULT NULL,
   `date_time` datetime DEFAULT current_timestamp(),
   `transaction_hash` varchar(150) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `token_transactions`
@@ -231,6 +292,26 @@ INSERT INTO `token_transactions` (`token_transaction_id`, `property_id`, `sender
 (3, 1, 3, 9, 2, '2023-07-22 20:06:31', '0xc13d7905be5c989378a945487cd2a1193627ae606009e28e296d48ddaec66162'),
 (4, 1, 3, 9, 3, '2023-07-22 20:07:13', '0xc13d7905be5c989378a945487cd2a1193627ae606009e28e296d48ddaec66163'),
 (5, 1, 3, 8, 5, '2023-07-22 20:08:17', '0xc13d7905be5c989378a945487cd2a1193627ae606009e28e296d48ddaec66164');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `token_value`
+--
+
+CREATE TABLE `token_value` (
+  `token_value_id` int(11) NOT NULL,
+  `token_id` int(11) DEFAULT NULL,
+  `token_value` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `token_value`
+--
+
+INSERT INTO `token_value` (`token_value_id`, `token_id`, `token_value`) VALUES
+(1, 1, 1000),
+(2, 2, 2000);
 
 -- --------------------------------------------------------
 
@@ -245,29 +326,49 @@ CREATE TABLE `user` (
   `lname` varchar(100) DEFAULT NULL,
   `user_password` varchar(100) DEFAULT NULL,
   `contact` varchar(100) DEFAULT NULL,
-  `user_role` int(11) DEFAULT NULL,
   `CNIC` varchar(100) DEFAULT NULL,
   `wallet_address` varchar(150) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `email`, `fname`, `lname`, `user_password`, `contact`, `user_role`, `CNIC`, `wallet_address`) VALUES
-(3, 'hamza@gmail.com', 'hamza', 'junaid', '$2a$10$RTS83rTyRB4MjabKbuIASeOwI6U553wAMVooo0S0YilOKXemSwmO.', '03312554581', 0, '12344567888', '0x746376428734'),
-(8, 'hamza2@gmail.com', 'hamza', 'junaid', '$2a$10$I02Qnp6.qaZcz6k.ylbSROcLZyL0UCbLd3ToU7dHFIo8Ro.imjNOe', '03312554581', 0, '12344567888', NULL),
-(9, 'zaki@gmail.com', 'zaki', 'junaid', '$2a$10$.3CVnijlQiQ9vL5DovxvR.CubUPiDFbCfn5iZEf41nAbGa00gorey', '03312554352', 1, '4210177399572', NULL);
+INSERT INTO `user` (`user_id`, `email`, `fname`, `lname`, `user_password`, `contact`, `CNIC`, `wallet_address`) VALUES
+(3, 'hamza4@gmail.com', 'hamza', 'junaid', '$2a$10$RTS83rTyRB4MjabKbuIASeOwI6U553wAMVooo0S0YilOKXemSwmO.', '03312554581', '12344567888', '0x746376428734'),
+(8, 'hamza2@gmail.com', 'hamza', 'junaid', '$2a$10$I02Qnp6.qaZcz6k.ylbSROcLZyL0UCbLd3ToU7dHFIo8Ro.imjNOe', '03312554581', '12344567888', '0x746376428734'),
+(9, 'zaki@gmail.com', 'zaki', 'junaid', '$2a$10$.3CVnijlQiQ9vL5DovxvR.CubUPiDFbCfn5iZEf41nAbGa00gorey', '03312554352', '4210177399572', NULL);
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `admin_user`
+--
+ALTER TABLE `admin_user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`payment_id`),
+  ADD KEY `fk_payment_from` (`payment_from`),
+  ADD KEY `fk_transaction_id` (`transaction_id`);
+
+--
+-- Indexes for table `payment_status`
+--
+ALTER TABLE `payment_status`
+  ADD PRIMARY KEY (`payment_status_id`);
+
+--
 -- Indexes for table `property`
 --
 ALTER TABLE `property`
-  ADD PRIMARY KEY (`property_id`);
+  ADD PRIMARY KEY (`property_id`),
+  ADD KEY `fk_tokens` (`token_id`);
 
 --
 -- Indexes for table `rent`
@@ -324,7 +425,9 @@ ALTER TABLE `token_buy_request`
   ADD PRIMARY KEY (`req_id`),
   ADD KEY `property_id` (`property_id`),
   ADD KEY `user_id` (`user_id`),
-  ADD KEY `status` (`status`);
+  ADD KEY `status` (`status`),
+  ADD KEY `fk_payment_status_id` (`payment_status`),
+  ADD KEY `fk_payment` (`payment_id`);
 
 --
 -- Indexes for table `token_holders`
@@ -343,6 +446,13 @@ ALTER TABLE `token_transactions`
   ADD KEY `sender_id` (`sender_id`);
 
 --
+-- Indexes for table `token_value`
+--
+ALTER TABLE `token_value`
+  ADD PRIMARY KEY (`token_value_id`),
+  ADD KEY `token_id` (`token_id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -351,6 +461,18 @@ ALTER TABLE `user`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `admin_user`
+--
+ALTER TABLE `admin_user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payment_status`
+--
+ALTER TABLE `payment_status`
+  MODIFY `payment_status_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `rent`
@@ -386,7 +508,7 @@ ALTER TABLE `smart_contracts`
 -- AUTO_INCREMENT for table `tokens`
 --
 ALTER TABLE `tokens`
-  MODIFY `token_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `token_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `token_buy_request`
@@ -401,6 +523,12 @@ ALTER TABLE `token_transactions`
   MODIFY `token_transaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
+-- AUTO_INCREMENT for table `token_value`
+--
+ALTER TABLE `token_value`
+  MODIFY `token_value_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
@@ -409,6 +537,19 @@ ALTER TABLE `user`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `payment`
+--
+ALTER TABLE `payment`
+  ADD CONSTRAINT `fk_payment_from` FOREIGN KEY (`payment_from`) REFERENCES `user` (`user_id`),
+  ADD CONSTRAINT `fk_transaction_id` FOREIGN KEY (`transaction_id`) REFERENCES `token_buy_request` (`req_id`);
+
+--
+-- Constraints for table `property`
+--
+ALTER TABLE `property`
+  ADD CONSTRAINT `fk_tokens` FOREIGN KEY (`token_id`) REFERENCES `tokens` (`token_id`);
 
 --
 -- Constraints for table `rent`
@@ -435,6 +576,8 @@ ALTER TABLE `smart_contracts`
 -- Constraints for table `token_buy_request`
 --
 ALTER TABLE `token_buy_request`
+  ADD CONSTRAINT `fk_payment` FOREIGN KEY (`payment_id`) REFERENCES `payment` (`payment_id`),
+  ADD CONSTRAINT `fk_payment_status_id` FOREIGN KEY (`payment_status`) REFERENCES `payment_status` (`payment_status_id`),
   ADD CONSTRAINT `token_buy_request_ibfk_1` FOREIGN KEY (`property_id`) REFERENCES `property` (`property_id`),
   ADD CONSTRAINT `token_buy_request_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
   ADD CONSTRAINT `token_buy_request_ibfk_3` FOREIGN KEY (`status`) REFERENCES `request_status` (`status_id`);
@@ -450,9 +593,15 @@ ALTER TABLE `token_holders`
 -- Constraints for table `token_transactions`
 --
 ALTER TABLE `token_transactions`
+  ADD CONSTRAINT `fk_admin_sender` FOREIGN KEY (`sender_id`) REFERENCES `admin_user` (`user_id`),
   ADD CONSTRAINT `token_transactions_ibfk_1` FOREIGN KEY (`property_id`) REFERENCES `property` (`property_id`),
-  ADD CONSTRAINT `token_transactions_ibfk_2` FOREIGN KEY (`receiver_id`) REFERENCES `user` (`user_id`),
-  ADD CONSTRAINT `token_transactions_ibfk_3` FOREIGN KEY (`sender_id`) REFERENCES `user` (`user_id`);
+  ADD CONSTRAINT `token_transactions_ibfk_2` FOREIGN KEY (`receiver_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `token_value`
+--
+ALTER TABLE `token_value`
+  ADD CONSTRAINT `token_value_ibfk_1` FOREIGN KEY (`token_id`) REFERENCES `tokens` (`token_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
